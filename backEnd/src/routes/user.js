56763e06fd33efd6a32615de6b8bd8a193c30e80
Mohamadreza golbaba <m.reza.golbaba@gmail.com>
2021-02-15 15:40:25 +0330

    First commit

---
+++ backEnd/src/routes/user.js
@@ -0,0 +1,14 @@
+const {signup} = require( "../controllers/user");
+const express = require('express');
+const router = express.Router();
+
+    router.post('/signin', (req,res) => {
+        res.status(200).json({
+            message:'hi from server'
+        })
+    });
+    router.post('/signup', signup);
+
+
+
+module.exports=router;
