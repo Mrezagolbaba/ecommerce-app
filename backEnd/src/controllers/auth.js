f26a4d301810002191ebab17847c194b045d375c
Mohamadreza golbaba <m.reza.golbaba@gmail.com>
2021-02-16 11:38:24 +0330

    change controller name and update sign in

---
+++ backEnd/src/controllers/auth.js
@@ -0,0 +1,67 @@
+const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+exports.signup = (req,res)=>{
+    User.findOne({email: req.body.email}, (error, user) => {
+        if(user) return res.status(400).json({
+            message:'User Already registered!'
+        });
+        const {
+            firstName,
+            lastName,
+            email,
+            password
+        } = req.body;
+        const newUser = new User({
+            firstName,
+            lastName,
+            email,
+            password,
+            userName:Math.random().toString()
+        });
+        newUser.save((error,data)=>{
+            console.log(error)
+            if(error){
+                return res.status(400).json({
+                    message:'Something went wrong'
+                })
+            }
+            if(data){
+                return res.status(201).json({
+                    message:'User created Successfully ...!'
+                })
+            }
+        })
+    })
+};
+exports.signin = (req,res) =>{
+    User.findOne({email : req.body.email})
+        .exec((error,user)=>{
+            if(error) return res.status(400).json({error});
+            if(user){
+                if(user.authenticate(req.body.password)){
+                    const token = jwt.sign({_id: user._id},process.env.JWT_SECRET,{expiresIn: '1h'})
+                    const {_id,firstName,lastName,email,role,fullName}=user;
+                    res.status(200).json({
+                        token,
+                        user:{
+                            _id,
+                            firstName,
+                            lastName,
+                            email,
+                            role,
+                            fullName
+                        }
+                    });
+                }else{
+                    return res.status(400).json({message:'Invalid Password'})
+                }
+            }else {
+                return res.status(400).json({message:'Something went wrong'})
+            }
+        })
+};
+exports.requireSignin = (req,res,next) =>{
+    const token = req.headers.authorization.split(" ")[1];
+    req.user = jwt.verify(token,process.env.JWT_SECRET);
+    next()
+}
