f588b2fd493debba7d8d991724c1b039f4cf2e1d
Mohamadreza golbaba <m.reza.golbaba@gmail.com>
2021-02-18 16:34:12 +0330

    BackEnd:Routes: update mistake auth user

--- backEnd/src/routes/auth.js
+++ backEnd/src/routes/auth.js
@@ -1,11 +1,1 @@
-const {signup,signin,requireSignin} = require( "../controllers/auth");
-const express = require('express');
-const router = express.Router();
-
-    router.post('/signin', signin);
-    router.post('/signup', signup);
-    // router.post('/profile',requireSignin, (req,res)=>{
-    //     res.status(200).json({user:'profile'})
-    // });
-
-module.exports=router;
+const {signup,signin} = require( "../controllers/auth");const express = require('express');const router = express.Router();router.post('/signin', signin);router.post('/signup', signup);// router.post('/profile',requireSignin, (req,res)=>{//     res.status(200).json({user:'profile'})// });module.exports=router;
