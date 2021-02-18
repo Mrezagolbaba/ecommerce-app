785b8a7ec9dbd3f30051ec929ca40c30ecee82e6
Mohamadreza golbaba <m.reza.golbaba@gmail.com>
2021-02-18 17:20:40 +0330

    BackEnd:Routh:add validation function for admin and user

--- backEnd/src/routes/admin/auth.js
+++ backEnd/src/routes/admin/auth.js
@@ -1,9 +1,10 @@
 const {signup,signin,requireSignin} = require( "../../controllers/admin/auth");
 const express = require('express');
+const {isRequestValidate,validateSignupRequest,validateSigninRequest} = require("../../validator/authValidator");
 const router = express.Router();
 
-    router.post('/admin/signin', signin);
-    router.post('/admin/signup', signup);
+    router.post('/admin/signin',validateSignupRequest,isRequestValidate, signin);
+    router.post('/admin/signup',validateSigninRequest,isRequestValidate, signup);
     // router.post('/profile',requireSignin, (req,res)=>{
     //     res.status(200).json({user:'profile'})
     // });
