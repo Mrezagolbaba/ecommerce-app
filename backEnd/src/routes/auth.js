ade664f42271ff25f59bb5a2decabcd55319b938
Mohamadreza golbaba <m.reza.golbaba@gmail.com>
2021-02-19 20:03:29 +0330

    remove unused function

--- backEnd/src/routes/auth.js
+++ backEnd/src/routes/auth.js
@@ -1,1 +1,8 @@
-const {validateSignupRequest,isRequestValidate,validateSigninRequest} =require( "../validator/authValidator");const {signup,signin} = require( "../controllers/auth");const express = require('express');const router = express.Router();router.post('/signin',validateSigninRequest,isRequestValidate, signin);router.post('/signup',validateSignupRequest,isRequestValidate, signup);// router.post('/profile',requireSignin, (req,res)=>{//     res.status(200).json({user:'profile'})// });module.exports=router;
+const {validateSignupRequest,isRequestValidate,validateSigninRequest} =require( "../validator/authValidator");
+const {signup,signin} = require( "../controllers/auth");
+const express = require('express');
+const router = express.Router();
+
+router.post('/signin',validateSigninRequest,isRequestValidate, signin);
+router.post('/signup',validateSignupRequest,isRequestValidate, signup);
+module.exports=router;
