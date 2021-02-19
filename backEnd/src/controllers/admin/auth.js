c16010610ef600737749cea6929a2da9cc782732
Mohamadreza golbaba <m.reza.golbaba@gmail.com>
2021-02-19 20:03:10 +0330

    create middleware for reused function

--- backEnd/src/controllers/admin/auth.js
+++ backEnd/src/controllers/admin/auth.js
@@ -61,8 +61,3 @@ exports.signin = (req,res) =>{
             }
         })
 };
-exports.requireSignin = (req,res,next) =>{
-    const token = req.headers.authorization.split(" ")[1];
-    req.user = jwt.verify(token,process.env.JWT_SECRET);
-    next()
-};
