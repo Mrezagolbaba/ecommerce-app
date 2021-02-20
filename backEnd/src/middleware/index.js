c1104189071bb46c5e249fac8279dfa42623000c
Mohamadreza golbaba <m.reza.golbaba@gmail.com>
2021-02-20 12:32:24 +0330

    create admin middleware

--- backEnd/src/middleware/index.js
+++ backEnd/src/middleware/index.js
@@ -1,7 +1,25 @@
 const jwt = require('jsonwebtoken');
 
 exports.requireSignin = (req,res,next) =>{
-    const token = req.headers.authorization.split(" ")[1];
-    req.user = jwt.verify(token,process.env.JWT_SECRET);
+    if(req.headers.authorization){
+        const token = req.headers.authorization.split(" ")[1];
+        req.user = jwt.verify(token,process.env.JWT_SECRET);
+        next()
+    }
+    return res.status(400).json({ message:'Authorization require!'})
+   
+}
+
+exports.userMiddleware = (req,res,next) =>{
+
+}
+exports.adminMiddleware = (req,res,next) =>{
+
+    if(req.user.role !== 'admin'){
+        return res.status(400).json({
+            message:'Access denied'
+        })
+    }
     next()
+    
 }
