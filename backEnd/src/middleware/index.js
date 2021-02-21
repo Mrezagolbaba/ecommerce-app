328386c467aac204b0bada93c430bb058ff8d5d1
Mohamadreza golbaba <m.reza.golbaba@gmail.com>
2021-02-21 17:24:51 +0330

    update middleware function

--- backEnd/src/middleware/index.js
+++ backEnd/src/middleware/index.js
@@ -4,20 +4,25 @@ exports.requireSignin = (req,res,next) =>{
     if(req.headers.authorization){
         const token = req.headers.authorization.split(" ")[1];
         req.user = jwt.verify(token,process.env.JWT_SECRET);
-        next()
+    }else{
+        return res.status(400).json({ message:'Authorization require!'})
     }
-    return res.status(400).json({ message:'Authorization require!'})
-   
+    next()   
 }
 
 exports.userMiddleware = (req,res,next) =>{
-
+    if(req.user.role !== 'user'){
+        return res.status(400).json({
+            message:'User access denied'
+        })
+    }
+    next()
 }
 exports.adminMiddleware = (req,res,next) =>{
 
     if(req.user.role !== 'admin'){
         return res.status(400).json({
-            message:'Access denied'
+            message:'Admin access denied'
         })
     }
     next()
