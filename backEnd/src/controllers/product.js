0b208fe1c66e1a5c8ab7c3b2f7c045bff7d0f3dc
Mohamadreza golbaba <m.reza.golbaba@gmail.com>
2021-02-22 17:01:27 +0330

    create product controller

---
+++ backEnd/src/controllers/product.js
@@ -0,0 +1,31 @@
+const Product = require('../models/product');
+const multer = require('multer');
+const slugify = require('slugify');
+exports.createProduct = (req, res) => {
+    const { name, price, description, category, quantity, createdBy } = req.body;
+    let productPictures = [];
+  
+    if (req.files.length > 0) {
+      productPictures = req.files.map((file) => {
+        return { img: file.location };
+      });
+    }
+  
+    const product = new Product({
+      name: name,
+      slug: slugify(name),
+      price,
+      quantity,
+      description,
+      productPictures,
+      category,
+      createdBy: req.user._id,
+    });
+
+    product.save()
+        .then((product) => {
+            return res.status(201).json({ product })
+        }).catch(function (error) {
+            res.status(400).json({ error })
+        });
+}
