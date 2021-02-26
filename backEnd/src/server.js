9fb4ca0654d584fbc63671a5c1b7e823fe45e219
Mohamadreza golbaba <m.reza.golbaba@gmail.com>
2021-02-26 14:43:38 +0330

    add file path in index

--- backEnd/src/server.js
+++ backEnd/src/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const env = require('dotenv');
 const mongoose = require('mongoose');
 const app = express();
+const path = require('path')
 
     //routes
     const authRoutes = require('./routes/auth');
@@ -25,6 +26,7 @@ const app = express();
    });
 
    app.use(express.json());
+   app.use('public',express.static(path.join(__dirname,'uploads')));
    app.use('/api',authRoutes);
    app.use('/api',adminRoutes);
    app.use('/api',categoryRoutes);
