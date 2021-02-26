147ad021c08acc026191ea192ab8c624c34aad1c
Mohamadreza golbaba <m.reza.golbaba@gmail.com>
2021-02-26 14:45:49 +0330

    add category image for model

--- backEnd/src/models/category.js
+++ backEnd/src/models/category.js
@@ -11,6 +11,9 @@ const categorySchema = new  mongoose.Schema({
         require:true,
         unique:true
     },
+    categoryImage:{
+        type:String
+    },
     parentId:{
         type:String,
         
