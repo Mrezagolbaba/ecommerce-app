5a2123dc9f554287c87f6519994d1cdda68c9926
Mohamadreza golbaba <m.reza.golbaba@gmail.com>
2021-02-26 14:46:03 +0330

    add upload for category

--- backEnd/src/routes/category.js
+++ backEnd/src/routes/category.js
@@ -2,9 +2,22 @@ const express = require('express');
 const {addCategory,getCategories} = require ('../controllers/category');
 const { requireSignin, adminMiddleware } = require('../middleware');
 const router = express.Router();
+const multer = require('multer');
+const shortid = require('shortid');
+const path = require('path');
 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.join(path.dirname(__dirname),'uploads'))
+    },
+    filename: function (req, file, cb) {
+      cb(null,shortid.generate() + '-' + file.originalname)
+    }
+  })
+  
+  var upload = multer({ storage })
 
-router.post('/category/create',requireSignin,adminMiddleware,addCategory);
+router.post('/category/create',requireSignin,adminMiddleware,upload.single('categoryImg'),addCategory);
 router.get('/category/getCategory', getCategories);
 
 module.exports=router;
