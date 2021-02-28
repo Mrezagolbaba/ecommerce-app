7cdd816dabfc600e590a56355b8c2e5e7dd46d06
Mohamadreza golbaba <m.reza.golbaba@gmail.com>
2021-02-28 11:02:28 +0330

    update action auth

--- admin/src/utils/actions/auth.js
+++ admin/src/utils/actions/auth.js
@@ -1,14 +1,12 @@
 import { authConstant } from "../constant"
+import axios from "../../helpers/axios";
 
 export const login = (user) =>{
-    return (dispatch)=>{
+    return async (dispatch)=>{
+        const res = await axios.post(`/admin/signin`)
         dispatch({
             type:authConstant.LOGIN_REQUEST,
             payload:{ ...user}
     })
     }
 }
-// export const login = (user) => ({
-//     type:authConstant.LOGIN_REQUEST,
-//     user
-// });
