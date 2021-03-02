702d19cf5b48b6f66630fbd4ac11db18c4ebdd14
Mohamadrez@ <m.reza.golbaba@gmail.com>
2021-03-02 14:16:57 +0330

    update action login

--- admin/src/utils/actions/auth.js
+++ admin/src/utils/actions/auth.js
@@ -2,11 +2,31 @@ import { authConstant } from "../constant"
 import axios from "../../helpers/axios";
 
 export const login = (user) =>{
+    console.log(user)
     return async (dispatch)=>{
-        const res = await axios.post(`/admin/signin`)
-        dispatch({
-            type:authConstant.LOGIN_REQUEST,
-            payload:{ ...user}
-    })
+        dispatch({type:authConstant.LOGIN_REQUEST})
+        const res = await axios.post(`/admin/signin`,{
+            ...user
+        })
+
+        if(res.status===200){
+            const {token,user} = res.data
+            localStorage.setItem('token',token)
+            dispatch({
+                type:authConstant.LOGIN_SUCCESS,
+                payload:{
+                    token,user
+                }
+            })
+        }else{
+            if(res.status===400){
+                dispatch({
+                    type:authConstant.LOGIN_FAILURE,
+                    payload:{
+                        error: res.data.error
+                    }
+                })
+            }
+        }
     }
 }
