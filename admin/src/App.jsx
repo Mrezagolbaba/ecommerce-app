8f4e34595f5a8ce4e741fe9d583ad247778a9dac
Mohamadrez@ <m.reza.golbaba@gmail.com>
2021-03-02 14:14:35 +0330

    add private route for redirect home

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -3,20 +3,21 @@ import './App.css';
 import Home from "./containers/Home";
 import Signin from "./containers/Signin";
 import Signup from "./containers/Signup";
+import PrivateRoute from "./components/HOC/privateRoute";
 
 
 function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Switch>
-          <Route path="/" exact component={Home}/>
-          <Route path="/signin" component={Signin}/>
-          <Route path="/signup" component={Signup}/>
-        </Switch>
-      </Router>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Router>
+                <Switch>
+                    <PrivateRoute path="/" exact component={Home}/>
+                    <Route path="/signin" component={Signin}/>
+                    <Route path="/signup" component={Signup}/>
+                </Switch>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
