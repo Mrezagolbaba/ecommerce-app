79c063b6bdb6a4b20c0d17f04a3015e3390860dd
Mohamadrez@ <m.reza.golbaba@gmail.com>
2021-03-03 15:42:16 +0330

    add sidebar to home page

--- admin/src/containers/Home/index.jsx
+++ admin/src/containers/Home/index.jsx
@@ -1,13 +1,24 @@
 import Layout from "../../components/Layout";
-import { Jumbotron } from "react-bootstrap";
-
+import {Col, Container, Jumbotron, Row} from "react-bootstrap";
+import './style.css'
 export default function Home() {
     return (
         <div>
             <Layout>
-                <Jumbotron style={{margin:'5rem',background:'#fff'}} className="text-center">
-                    <h1>Welcome to Admin Dashbpard</h1>
-                </Jumbotron>
+                <Container fluid >
+                    <Row >
+                        <Col md={10} style={{marginLeft:'auto'}}>
+                            container
+                        </Col>
+                        <Col md={2} className='sidebar'>
+                            side bar
+                        </Col>
+                    </Row>
+                </Container>
+
+                {/*<Jumbotron style={{margin:'5rem',background:'#fff'}} className="text-center">*/}
+                {/*    <h1>Welcome to Admin Dashbpard</h1>*/}
+                {/*</Jumbotron>*/}
             </Layout>
         </div>
     )
