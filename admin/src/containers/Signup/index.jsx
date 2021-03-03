6b7669cc8034e6455f2412d6c8bd2127b16f391b
Mohamadrez@ <m.reza.golbaba@gmail.com>
2021-03-03 15:44:34 +0330

    update signup component

--- admin/src/containers/Signup/index.jsx
+++ admin/src/containers/Signup/index.jsx
@@ -5,23 +5,43 @@ import { login } from "../../utils/actions";
 import {useDispatch, useSelector} from "react-redux";
 import { useState } from "react";
 import {Redirect} from "react-router-dom";
+import {signup} from "../../utils/actions/user";
 export default function Signup() {
     const [email,setEmail]= useState('')
     const [password,setPassword]= useState('')
+    const [firstName,setFirstName]= useState('')
+    const [lastName,setLastName]= useState('')
+    const [error,setError]= useState('')
+    const auth = useSelector(state => state.auth)
+    const user = useSelector(state => state.user)
+    const dispatch = useDispatch()
+    const userSignUp = (e) => {
+        e.preventDefault();
+        let user ={
+            email,password,firstName,lastName
+        }
+        dispatch(signup(user))
+    }
+    if(auth.authenticate){
+        return <Redirect to={`/`}/>
+    }
+    if(user.loading){
+        return <p>>loading...!</p>
+    }
     return (
         <Layout>
             <Container>
                 <Row style={{ marginTop: '50px' }}>
                     <Col md={{ span: 6, offset: 3 }}>
-                        <Form >
+                        <Form onSubmit={userSignUp} >
                             <Row>
                                 <Col md={6}>
                                     <Input
                                         label="First Name"
                                         placeholder="First Name"
-                                        value=""
+                                        value={firstName}
                                         type="text"
-                                        onChange={()=>{}}
+                                        onChange={(e)=>setFirstName(e.target.value)}
                                         // errorMessage=" First Name is required!"
                                     />
                                 </Col>
@@ -29,9 +49,9 @@ export default function Signup() {
                                     <Input
                                         label="Last Name"
                                         placeholder="Last Name"
-                                        value=""
+                                        value={lastName}
                                         type="text"
-                                        onChange={()=>{}}
+                                        onChange={(e)=>setLastName(e.target.value)}
                                         // errorMessage=" First Name is required!"
                                     />
                                 </Col>
@@ -39,18 +59,18 @@ export default function Signup() {
                             <Input
                                 label="Email"
                                 placeholder="Email"
-                                value=""
+                                value={email}
                                 type="email"
-                                onChange={()=>{}}
+                                onChange={(e)=>setEmail(e.target.value)}
                                 // errorMessage=" First Name is required!"
                             />
 
                             <Input
                                 label="Password"
                                 placeholder="Password"
-                                value=""
+                                value={password}
                                 type="password"
-                                onChange={()=>{}}
+                                onChange={(e)=>setPassword(e.target.value)}
                                 // errorMessage=" First Name is required!"
                             />
                             <Button variant="primary" type="submit" >
