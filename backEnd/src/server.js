abdb6d177107fe71295c83577da5cbf3522f7275
Mohamadrez@ <m.reza.golbaba@gmail.com>
2021-03-04 09:43:51 +0330

    add cors

--- backEnd/src/server.js
+++ backEnd/src/server.js
@@ -3,6 +3,7 @@ const env = require('dotenv');
 const mongoose = require('mongoose');
 const app = express();
 const path = require('path')
+const cors = require('cors')
 
     //routes
     const authRoutes = require('./routes/auth');
@@ -24,7 +25,19 @@ const path = require('path')
     ).then(() => {
         console.log('database connected')
    });
+const corsOpts = {
+    origin: '*',
 
+    methods: [
+        'GET',
+        'POST',
+    ],
+
+    allowedHeaders: [
+        'Content-Type',
+    ],
+};
+    app.use(cors(corsOpts));
    app.use(express.json());
    app.use('public',express.static(path.join(__dirname,'uploads')));
    app.use('/api',authRoutes);
