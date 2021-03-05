517156f562192d889f7686fa036b333dce4e3159
Mohamadrez@ <m.reza.golbaba@gmail.com>
2021-03-05 09:15:26 +0330

    update home

--- admin/src/containers/Home/index.jsx
+++ admin/src/containers/Home/index.jsx
@@ -1,24 +1,13 @@
 import Layout from "../../components/Layout";
-import {Col, Container, Jumbotron, Row} from "react-bootstrap";
 import './style.css'
+import {Jumbotron} from "react-bootstrap";
 export default function Home() {
     return (
         <div>
-            <Layout>
-                <Container fluid >
-                    <Row >
-                        <Col md={10} style={{marginLeft:'auto'}}>
-                            container
-                        </Col>
-                        <Col md={2} className='sidebar'>
-                            side bar
-                        </Col>
-                    </Row>
-                </Container>
-
-                {/*<Jumbotron style={{margin:'5rem',background:'#fff'}} className="text-center">*/}
-                {/*    <h1>Welcome to Admin Dashbpard</h1>*/}
-                {/*</Jumbotron>*/}
+            <Layout sidebar>
+                <Jumbotron style={{margin:'5rem',background:'#fff'}} className="text-center">
+                    <h1>Welcome to Admin Dashbpard</h1>
+                </Jumbotron>
             </Layout>
         </div>
     )
