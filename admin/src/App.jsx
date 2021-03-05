c78ddfeb93f3d2fda4791ddcc4e44e4476e443c9
Mohamadrez@ <m.reza.golbaba@gmail.com>
2021-03-05 09:14:56 +0330

    update route and add product and orders

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -7,6 +7,8 @@ import PrivateRoute from "./components/HOC/privateRoute";
 import {useEffect} from "react";
 import {isUserLoggedIn} from "./utils/actions";
 import {useDispatch, useSelector} from "react-redux";
+import Products from "./containers/Products";
+import Orders from "./containers/Orders";
 
 
 function App() {
@@ -21,6 +23,8 @@ function App() {
         <div className="App">
                 <Switch>
                     <PrivateRoute path="/" exact component={Home}/>
+                    <PrivateRoute path="/products"  component={Products}/>
+                    <PrivateRoute path="/orders"  component={Orders}/>
                     <Route path="/signin" component={Signin}/>
                     <Route path="/signup" component={Signup}/>
                 </Switch>
