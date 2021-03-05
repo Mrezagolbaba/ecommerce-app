b4b8f38f090e625c06bcdd81b98d32679f54d41d
Mohamadrez@ <m.reza.golbaba@gmail.com>
2021-03-05 09:15:47 +0330

    add sidebar to layout

--- admin/src/components/Layout/index.jsx
+++ admin/src/components/Layout/index.jsx
@@ -1,10 +1,31 @@
 import Header from "../Header";
-import { Container } from 'react-bootstrap';
+import {Col, Container, Row} from 'react-bootstrap';
+import {NavLink} from "react-router-dom";
 export default function Layout(props) {
     return (
       <>
         <Header/>
-          {props.children}
+          {props.sidebar?
+              <Container fluid >
+                  <Row >
+                      <Col md={10} style={{marginLeft:'auto'}}>
+                          {props.children}
+                      </Col>
+                      <Col md={2} className='sidebar'>
+                          <div dir="RTL">
+                              <ul style={{padding:'0'}}>
+                                  <li><NavLink to='/'>خانه</NavLink></li>
+                                  <li><NavLink to='/products'>محصولات</NavLink></li>
+                                  <li><NavLink to='/orders'>سفارشات</NavLink></li>
+                              </ul>
+                          </div>
+                      </Col>
+                  </Row>
+              </Container>
+              :
+              props.children
+          }
+
       </>
     )
 }
