549dbf724619efd8fb8370b8d899d42ef4c38ef8
Mohamadrez@ <m.reza.golbaba@gmail.com>
2021-03-06 23:15:10 +0330

    add category route

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -9,6 +9,7 @@ import {isUserLoggedIn} from "./utils/actions";
 import {useDispatch, useSelector} from "react-redux";
 import Products from "./containers/Products";
 import Orders from "./containers/Orders";
+import Category from "./containers/Category";
 
 
 function App() {
@@ -25,6 +26,7 @@ function App() {
                     <PrivateRoute path="/" exact component={Home}/>
                     <PrivateRoute path="/products"  component={Products}/>
                     <PrivateRoute path="/orders"  component={Orders}/>
+                    <PrivateRoute path="/category"  component={Category}/>
                     <Route path="/signin" component={Signin}/>
                     <Route path="/signup" component={Signup}/>
                 </Switch>
