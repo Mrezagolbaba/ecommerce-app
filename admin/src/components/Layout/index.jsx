a6ae98786f6ac151edfd222cb64cbb82959124eb
Mohamadrez@ <m.reza.golbaba@gmail.com>
2021-03-06 23:14:24 +0330

    add new link for sidebar

--- admin/src/components/Layout/index.jsx
+++ admin/src/components/Layout/index.jsx
@@ -8,14 +8,15 @@ export default function Layout(props) {
           {props.sidebar?
               <Container fluid >
                   <Row >
-                      <Col md={10} style={{marginLeft:'auto'}}>
+                      <Col md={10} style={{marginRight:'auto'}}>
                           {props.children}
                       </Col>
                       <Col md={2} className='sidebar'>
                           <div dir="RTL">
-                              <ul style={{padding:'0'}}>
+                              <ul style={{padding:'0',textAlign:'right'}}>
                                   <li><NavLink to='/'>خانه</NavLink></li>
                                   <li><NavLink to='/products'>محصولات</NavLink></li>
+                                  <li><NavLink to='/category'>دسته بندی</NavLink></li>
                                   <li><NavLink to='/orders'>سفارشات</NavLink></li>
                               </ul>
                           </div>
