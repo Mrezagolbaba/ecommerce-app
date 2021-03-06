4ea9fdc2096bcea7766be416c2ae74e78a49dae8
Mohamadrez@ <m.reza.golbaba@gmail.com>
2021-03-06 23:12:07 +0330

    rename reducer

--- admin/src/utils/reducers/auth.Reducer.js
+++ admin/src/utils/reducers/auth.Reducer.js
@@ -17,6 +17,7 @@ const initialState = {
 };
 
 const authReducer = (state=initialState, action) => {
+    console.log(action)
     switch (action.type){
         case authConstant.LOGIN_REQUEST:
             state = {
--- admin/src/utils/reducers/index.js
+++ admin/src/utils/reducers/index.js
@@ -1,9 +1,16 @@
-import { combineReducers } from 'redux'
-import authReducer from './auth'
-import userReducer from './user'
+import { combineReducers } from 'redux';
+import authReducer from './auth.Reducer';
+import userReducer from './user.Reducer';
+import productReducer from './product.Reducer';
+import orderReducer from './orders.Reducer';
+import categoryReducer from './category.Reducer';
 
 const rootReduces = combineReducers({
      auth:authReducer,
-     user:userReducer
+     user:userReducer,
+     category:categoryReducer,
+     product:productReducer,
+     order:orderReducer,
+
 });
 export default rootReduces;
