195a4de6272133534f7291479e5ac123e6c7df3e
Mohamadrez@ <m.reza.golbaba@gmail.com>
2021-03-07 11:19:39 +0330

    update category reducer and action for add new one

--- admin/src/utils/reducers/category.Reducer.js
+++ admin/src/utils/reducers/category.Reducer.js
@@ -6,6 +6,30 @@ const initialState = {
     error:null,
 };
 
+const buildNewCategories =(parentId,categories,category)=>{
+    let myCategories=[];
+    categories.map((i)=>{
+        if(i._id === parentId){
+            myCategories.push({
+                ...i,
+                children:i.children&&i.children.length > 0? buildNewCategories(parentId,[...i.children,{
+                    _id:category._id,
+                    name:category.name,
+                    slug: category.slug,
+                    parentId:category.parentId,
+                    children:category.children
+                }],category) : []
+            })
+        }else {
+            myCategories.push({
+                ...i,
+                children:i.children&&i.children.length > 0? buildNewCategories(parentId,i.children,category) : []
+            })
+        }
+    });
+    return myCategories;
+}
+
 const categoryReducer = (state= initialState, action) => {
     console.log(action)
     switch (action.type){
@@ -22,8 +46,11 @@ const categoryReducer = (state= initialState, action) => {
             }
             break;
         case categoryConstant.ADD_NEW_CATEGORY_SUCCESS:
+            const category = action.payload.category;
+            const updateCategories= buildNewCategories(category.parentId,state.categories,category)
             state = {
                 ...state,
+                categories:updateCategories,
                 loading:false,
             };
             break;
