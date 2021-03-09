ccbcaee105e75adf2d193bffe9f62f7c0d555b65
Mohamadrez@ <m.reza.golbaba@gmail.com>
2021-03-09 16:16:35 +0330

    add initialData.js for route

---
+++ backEnd/src/routes/admin/initialData.js
@@ -0,0 +1,10 @@
+const {signup,signin,signout} = require( "../../controllers/admin/auth");
+const express = require('express');
+const router = express.Router();
+
+router.post('/initialData',(res,req) => {
+
+});
+
+
+module.exports=router;
