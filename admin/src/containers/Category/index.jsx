b83e8cdb1e3196f55a0ab7bf0c4ccd1b2f67f3b9
Mohamadrez@ <m.reza.golbaba@gmail.com>
2021-03-09 16:14:10 +0330

    update category and function modal

--- admin/src/containers/Category/index.jsx
+++ admin/src/containers/Category/index.jsx
@@ -1,10 +1,11 @@
 import {useEffect, useState} from "react";
 import Layout from "../../components/Layout";
-import {Button, Col, Container, Modal, Row} from "react-bootstrap";
+import {Button, Col, Container, Row} from "react-bootstrap";
 import './styles.css'
 import {useDispatch, useSelector} from "react-redux";
 import {addCategory, getAllCategory} from "../../utils/actions";
 import {Input} from "../../components/UI/Input";
+import Modal from "../../components/UI/Modal";
 
 const Category = (props) => {
     const [show, setShow] = useState(false);
@@ -12,99 +13,90 @@ const Category = (props) => {
     const [categoryParentId, setCategoryParentId] = useState('');
     const [categoryImage, setCategoryImage] = useState('');
 
-    const category = useSelector ( state => state.category)
+    const category = useSelector(state => state.category)
 
     const dispatch = useDispatch()
 
-    useEffect(()=>{
-        dispatch(getAllCategory())
-    },[])
-
-
     const handleClose = () => {
         const form = new FormData()
-        form.append('name',categoryName)
-        form.append('parentId',categoryParentId)
-        form.append('categoryImg',categoryImage)
+        form.append('name', categoryName)
+        form.append('parentId', categoryParentId)
+        form.append('categoryImg', categoryImage)
         dispatch(addCategory(form))
+        setCategoryName('');
+        setCategoryParentId('')
         setShow(false);
     }
     const handleShow = () => setShow(true);
 
-    const renderCategories = (categories) =>{
+    const renderCategories = (categories) => {
         const myCategories = []
-        categories.map((i)=>{
+        categories.map((i) => {
             myCategories.push(
                 <li key={i.name}>
                     {i.name}
-                    {i.children.length > 0 ?(<ul>{renderCategories(i.children)}</ul>): null}
+                    {i.children.length > 0 ? (<ul>{renderCategories(i.children)}</ul>) : null}
                 </li>
-
             )
         })
         return myCategories
 
     }
-    const createCategoryList = (categories,options=[])=>{
-        categories.map((i)=>{
-            options.push({value:i._id,name:i.name})
-            if(i.children.length>0){
-                createCategoryList(i.children,options)
+    const createCategoryList = (categories, options = []) => {
+        categories.map((i) => {
+            options.push({value: i._id, name: i.name})
+            if (i.children.length > 0) {
+                createCategoryList(i.children, options)
             }
 
         })
         return options
     }
-    const handleCategoryImage = (e) =>{
+    const handleCategoryImage = (e) => {
         setCategoryImage(e.target.files[0])
     }
     return (
         <Layout sidebar>
-           <Container className='rtl'>
-               <Row>
-                   <Col md={12}>
+            <Container className='rtl'>
+                <Row>
+                    <Col md={12}>
                         <div className="title-page">
-                            <Button size="sm" variant="outline-success" onClick={handleShow}> اضافه کردن دسته بندی جدید</Button>
+                            <Button size="sm" variant="outline-success" onClick={handleShow}> اضافه کردن دسته بندی
+                                جدید</Button>
                             <h3>دسته بندی</h3>
                         </div>
-                   </Col>
-               </Row>
-               <Row>
-                   <Col md={12}>
-                       <ul>
-                           {category?.categories!==undefined && renderCategories(category.categories)}
-                       </ul>
-                   </Col>
-               </Row>
-           </Container>
-            <Modal show={show} onHide={handleClose}>
-                <Modal.Header closeButton>
-                    <Modal.Title>اضافه کردن دسته بندی جدید</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <Input
-                        value={categoryName}
-                        placeholder={'نام دسته بندی'}
-                        onChange={(e)=>setCategoryName(e.target.value)}
-                    />
-                    <select
-                        value={categoryParentId}
-                        className='form-control'
-                        onChange={(e)=>setCategoryParentId(e.target.value)}>
-                        <option>انتخاب دسته بندی</option>
-                        {category?.categories!==undefined &&
-                            createCategoryList(category.categories).map(option=>
-                                <option key={option.value} value={option.value}>{option.name}</option>
-                            )
-                        }
-                    </select>
-                    <input type='file' name='categoryImage' onChange={handleCategoryImage}/>
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="primary" onClick={handleClose}>
-                        Save Changes
-                    </Button>
-                </Modal.Footer>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col md={12}>
+                        <ul>
+                            {category?.categories !== undefined && renderCategories(category.categories)}
+                        </ul>
+                    </Col>
+                </Row>
+            </Container>
+            <Modal
+                show={show}
+                handleClose={handleClose}
+                modalTitle='اضافه کردن دسته بندی جدید'
+            >
+                <Input
+                    value={categoryName}
+                    placeholder={'نام دسته بندی'}
+                    onChange={(e) => setCategoryName(e.target.value)}
+                />
+                <select
+                    value={categoryParentId}
+                    className='form-control'
+                    onChange={(e) => setCategoryParentId(e.target.value)}>
+                    <option>انتخاب دسته بندی</option>
+                    {category?.categories !== undefined &&
+                    createCategoryList(category.categories).map(option =>
+                        <option key={option.value} value={option.value}>{option.name}</option>
+                    )
+                    }
+                </select>
+                <input type='file' name='categoryImage' onChange={handleCategoryImage}/>
             </Modal>
         </Layout>
     );
