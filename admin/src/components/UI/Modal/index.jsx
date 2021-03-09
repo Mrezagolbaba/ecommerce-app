f1cfa5df4551c85cd01fc051360e2e13094cba12
Mohamadrez@ <m.reza.golbaba@gmail.com>
2021-03-09 16:12:12 +0330

    create modal component

---
+++ admin/src/components/UI/Modal/index.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {Button, Modal,} from "react-bootstrap";
+import {Input} from "../Input";
+import Layout from "../../Layout";
+
+const NewModal = (props) => {
+    return (
+        <Modal show={props.show} onHide={props.handleClose}>
+            <Modal.Header>
+                <Button variant='outline-info' onClick={props.handleClose}> x</Button>
+                <Modal.Title>{props.modalTitle}</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                {props.children}
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="primary" onClick={props.handleClose}>
+                    ذخیره
+                </Button>
+            </Modal.Footer>
+        </Modal>
+    );
+};
+
+export default NewModal;
