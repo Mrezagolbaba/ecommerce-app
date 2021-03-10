f0b1a64e123d3afe5797775457d9f9eb07d46a41
Mohamadrez@ <m.reza.golbaba@gmail.com>
2021-03-10 18:12:23 +0330

    add route and controller initial data

--- backEnd/src/routes/admin/initialData.js
+++ backEnd/src/routes/admin/initialData.js
@@ -1,10 +1,8 @@
-const {signup,signin,signout} = require( "../../controllers/admin/auth");
 const express = require('express');
+const {initialData} = require("../../controllers/admin/initialData");
 const router = express.Router();
 
-router.post('/initialData',(res,req) => {
-
-});
+router.post('/initialData',initialData);
 
 
 module.exports=router;
