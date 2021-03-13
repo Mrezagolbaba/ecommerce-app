d3a3ae0c4700849ab0e1cdbcd3c4d18ae7b9824d
Mohamadrez@ <m.reza.golbaba@gmail.com>
2021-03-13 14:34:35 +0330

    update static file

--- backEnd/src/server.js
+++ backEnd/src/server.js
@@ -40,7 +40,7 @@ const corsOpts = {
 };
     app.use(cors(corsOpts));
    app.use(express.json());
-   app.use('public',express.static(path.join(__dirname,'uploads')));
+   app.use('/public',express.static(path.join(__dirname,'uploads')));
    app.use('/api',authRoutes);
    app.use('/api',adminRoutes);
    app.use('/api',categoryRoutes);
