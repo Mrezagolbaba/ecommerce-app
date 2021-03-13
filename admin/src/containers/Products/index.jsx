6631f8acc0b8e812cc68fa6b43a2bac112eab40f
Mohamadrez@ <m.reza.golbaba@gmail.com>
2021-03-13 14:32:20 +0330

    update style and add function details

--- admin/src/containers/Products/index.jsx
+++ admin/src/containers/Products/index.jsx
@@ -5,6 +5,8 @@ import {addProduct, getAllCategory} from "../../utils/actions";
 import {Input} from "../../components/UI/Input";
 import {useState} from "react";
 import Modal from "../../components/UI/Modal";
+import './styles.css'
+import {generatePublicUrl} from "../../urlConfig";
 
 const Products = () => {
     const [show, setShow] = useState(false);
@@ -15,6 +17,8 @@ const Products = () => {
     const [categoryId, setCategoryId] = useState('');
     const [productPicture, setProductPicture] = useState([]);
     const [categoryParentId, setCategoryParentId] = useState('');
+    const [showProductDetails, setShowProductDetails] = useState(false);
+    const [productDetails, setProductDetails] = useState(null);
     const dispatch = useDispatch()
     const category = useSelector(state => state.category)
     const product = useSelector(state => state.product)
@@ -35,7 +39,6 @@ const Products = () => {
     const handleProductImage = (e) => {
         setProductPicture([...productPicture, e.target.files[0]])
     }
-    console.log(productPicture)
 
     const createCategoryList = (categories, options = []) => {
         categories.map((i) => {
@@ -51,28 +54,24 @@ const Products = () => {
 
     const renderProduct = () => {
         return (
-            <Table responsive variant='dark' striped>
+            <Table responsive variant='dark' striped className='table-product'>
                 <thead>
                 <tr>
-                    <th>Name</th>
-                    <th>Price</th>
-                    <th>Quantity</th>
-                    <th>Description</th>
-                    <th>Product Picture</th>
-                    <th>Category</th>
+                    <th>دسته بندی</th>
+                    <th>تعداد</th>
+                    <th>قیمت</th>
+                    <th>نام</th>
                 </tr>
                 </thead>
                 <tbody>
                 {
                     product.products.length > 0 ?
                         product.products.map(product =>
-                            <tr>
-                                <td>{product.name}</td>
-                                <td>{product.price}</td>
+                            <tr onClick={()=>showProductDetailsModal(product)}>
+                                <td>{product.category?.name}</td>
                                 <td>{product.quantity}</td>
-                                <td>{product.description}</td>
-                                <td>{product.productPicture}</td>
-                                <td>{product.category}</td>
+                                <td>{product.price}</td>
+                                <td>{product.name}</td>
                             </tr>
                         ) : null
                 }
@@ -80,26 +79,8 @@ const Products = () => {
             </Table>
         )
     }
-
-    return (
-        <Layout sidebar>
-            <Container className='rtl'>
-                <Row>
-                    <Col md={12}>
-                        <div className="title-page">
-                            <Button size="sm" variant="outline-success" onClick={handleShow}> اضافه کردن محصول
-                                جدید</Button>
-                            <h3> محصولات</h3>
-                        </div>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        {renderProduct()}
-                    </Col>
-
-                </Row>
-            </Container>
+    const renderAddProduct = () =>{
+        return(
             <Modal
                 show={show}
                 handleClose={handleClose}
@@ -144,6 +125,96 @@ const Products = () => {
                 }
                 <input type='file' name='productPicture' onChange={handleProductImage}/>
             </Modal>
+        )
+    }
+    const handleCloseProductDetailsModal = () =>{
+        setShowProductDetails(false)
+    }
+     const showProductDetailsModal= (product) =>{
+        setProductDetails(product)
+        setShowProductDetails(true)
+         console.log(product)
+    }
+    const renderProductDetailsModal = () => {
+        if(!productDetails){
+            return null
+        }
+        return(
+            <Modal
+                show={showProductDetails}
+                handleClose={handleCloseProductDetailsModal}
+                modalTitle='اطلاعات محصول'
+                size='lg'
+            >
+                <div className='text-right'>
+                    <Row>
+                        <Col md={6}>
+                            <label className='title-product'>قیمت </label>
+                            <p>{productDetails.price}</p>
+                        </Col>
+                        <Col md={6}>
+                            <label className='title-product'>نام محصول</label>
+                            <p>{productDetails.name}</p>
+                        </Col>
+
+                    </Row>
+                    <Row>
+                        <Col md={6}>
+                            <label className='title-product'>تعداد </label>
+                            <p>{productDetails.quantity}</p>
+                        </Col>
+                        <Col md={6}>
+                            <label className='title-product'>دسته بندی </label>
+                            <p>{productDetails.category?.name}</p>
+                        </Col>
+
+                    </Row>
+                    <Row>
+                        <Col md={12}>
+                            <label className='title-product'>توضیحات </label>
+                            <p>{productDetails.description}</p>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col >
+                            <label className='title-product'>تصاویر</label>
+                            <div style={{display:'flex'}}>
+                                {productDetails&&productDetails.productPictures.map((pic)=>{
+                                    return(
+                                        <div className='product-image'>
+                                            <img src={generatePublicUrl(pic.img)}/>
+                                        </div>
+                                    )})}
+                            </div>
+
+                        </Col>
+                    </Row>
+
+                </div>
+            </Modal>
+        )
+    }
+    return (
+        <Layout sidebar>
+            <Container className='rtl'>
+                <Row>
+                    <Col md={12}>
+                        <div className="title-page">
+                            <Button size="sm" variant="outline-success" onClick={handleShow}> اضافه کردن محصول
+                                جدید</Button>
+                            <h3> محصولات</h3>
+                        </div>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        {renderProduct()}
+                    </Col>
+
+                </Row>
+            </Container>
+            {renderAddProduct()}
+            {renderProductDetailsModal()}
         </Layout>
     );
 };
