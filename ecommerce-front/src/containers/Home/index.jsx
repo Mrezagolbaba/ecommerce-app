096caba1a075681940c7064f68a9cda45c147910
Mohamadrez@ <m.reza.golbaba@gmail.com>
2021-03-19 22:03:09 +0330

    add home page

---
+++ ecommerce-front/src/containers/Home/index.jsx
@@ -0,0 +1,12 @@
+import {Header} from "../../components/Header";
+import {MenuHeader} from "../../components/MenuHeader";
+
+export const Home = () => {
+    return (
+        <div>
+            <Header/>
+            <MenuHeader/>
+            Home
+        </div>
+    );
+};
