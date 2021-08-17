f704e97a4fa5bc1066d25d60f5039e879b451f69
Mohamadrez@ <m.reza.golbaba@gmail.com>
2021-08-17 23:15:30 +0430

    front:  add product action

--- admin/src/utils/actions/product.action.js
+++ admin/src/utils/actions/product.action.js
@@ -1,8 +1,8 @@
-import axios from "../../helpers/axios";
+import axiosInstance from "../../helpers/axios";
 
 export const addProduct = (form) =>{
     return async dispatch =>{
-        const res = await axios.post(`product/create`,form)
-        console.log(form,res)
+        const res = await axiosInstance.post(`product/create`,form)
+        console.log('add product',form,res)
     }
 }
