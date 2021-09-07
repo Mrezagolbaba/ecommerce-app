8b9d73a1c332ee0da88c0de5e97c5b0d2e55d298
Mohamadrez@ <m.reza.golbaba@gmail.com>
2021-09-07 07:35:12 +0430

    Backend:  update authentication time token

--- backEnd/src/controllers/admin/auth.js
+++ backEnd/src/controllers/admin/auth.js
@@ -46,9 +46,9 @@ exports.signin = (req, res) => {
             if (error) return res.status(400).json({ error });
             if (user) {
                 if (user.authenticate(req.body.password) && user.role === 'admin') {
-                    const token = jwt.sign({ _id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1d' })
+                    const token = jwt.sign({ _id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1m' })
                     const { _id, firstName, lastName, email, role, fullName } = user;
-                    res.cookie('token',token,{expiresIn:'1d'});
+                    res.cookie('token',token,{expiresIn:'1m'});
                     res.status(200).json({
                         token,
                         user: {
