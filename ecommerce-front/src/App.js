97e25e4177201f77c581d3583ce33d99ce97a236
Mohamadrez@ <m.reza.golbaba@gmail.com>
2021-09-09 10:19:37 +0430

    front:  add app route

--- ecommerce-front/src/App.js
+++ ecommerce-front/src/App.js
@@ -1,11 +1,18 @@
-
-import './App.css';
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
+import {Product} from "./containers/Product";
 import {Home} from "./containers/Home";
+import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-        <Home/>
+        <Router>
+            <Switch>
+                <Route exact path="/" component={Home}/>
+                <Route  path="/:slug" component={Product}/>
+            </Switch>
+        </Router>
     </div>
   );
 }
