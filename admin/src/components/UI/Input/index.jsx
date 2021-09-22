b3ecc84d549e66bed7b8f98e77331e6e1747b4d6
Mohamadrez@ <m.reza.golbaba@gmail.com>
2021-09-22 10:27:04 +0330

    front:  add condition to input component

--- admin/src/components/UI/Input/index.jsx
+++ admin/src/components/UI/Input/index.jsx
@@ -3,7 +3,7 @@ import './styles.css'
 export function Input(props) {
     return (
         <Form.Group className='input-component'>
-            <Form.Label>{props.label}</Form.Label>
+            {props.label&&<Form.Label>{props.label}</Form.Label>}
             <Form.Control
                 type={props.type}
                 placeholder={props.placeholder}
