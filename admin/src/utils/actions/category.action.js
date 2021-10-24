0b4d569e2d158e2125eef3f2a2698530c1d22d23
Mohamadrez@ <m.reza.golbaba@gmail.com>
2021-10-25 00:01:33 +0330

    admin: update add category function action

--- admin/src/utils/actions/category.action.js
+++ admin/src/utils/actions/category.action.js
@@ -26,16 +26,21 @@ export const addCategory = (form) => {
     dispatch({ type: categoryConstant.ADD_NEW_CATEGORY_REQUEST });
     const res = await axios.post(`category/create`, form);
     console.log("res", res);
-    if (res.status === 201) {
-      dispatch({
-        type: categoryConstant.ADD_NEW_CATEGORY_SUCCESS,
-        payload: { category: res.data.category },
-      });
-    } else {
-      dispatch({
-        type: categoryConstant.ADD_NEW_CATEGORY_FAILURE,
-        payload: { error: res.data.error },
-      });
+    //TODO add sub categories generate error
+    try {
+      if (res.status === 201) {
+        dispatch({
+          type: categoryConstant.ADD_NEW_CATEGORY_SUCCESS,
+          payload: { category: res.data.category },
+        });
+      } else {
+        dispatch({
+          type: categoryConstant.ADD_NEW_CATEGORY_FAILURE,
+          payload: { error: res.data.error },
+        });
+      }
+    } catch (error) {
+      console.log("error", error);
     }
   };
 };
